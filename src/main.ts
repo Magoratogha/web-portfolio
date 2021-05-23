b004f95588f4b523270c39508680d1f64cddfae4
Santiago Gómez Grajales <magoratoga@gmail.com>
2021-05-23 09:57:39 -0500

    Add smooth scrolling feature to Safari browser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// @ts-ignore
+import * as smoothScroll from 'smoothscroll-polyfill';
+
+smoothScroll.polyfill();
 
 if (environment.production) {
   enableProdMode();
